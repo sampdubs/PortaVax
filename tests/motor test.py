611e3100657d0e5a02c359aec473cebb7e0731e7
sampdubs <sammypw1@gmail.com>
2022-06-29 13:35:47 -0400

    Add motor test

---
+++ tests/motor test.py
@@ -0,0 +1,14 @@
+from math import sin
+from gpiozero import Motor
+
+m = Motor(20, 21)
+def drive(speed):
+    if speed > 0:
+        m.forward(speed)
+    else:
+        m.backward(-speed)
+
+t = 0
+while True:
+    drive(sin(t))
+    t += 0.01
