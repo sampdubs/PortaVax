cb8dd81fc0f30622432db36ad316560f4ed910dd
sampdubs <sammypw1@gmail.com>
2022-06-30 13:35:48 -0400

    Formatting

--- tests/voltage.py
+++ tests/voltage.py
@@ -8,7 +8,7 @@ bus = smbus.SMBus(1)
 input_voltage = 5
 
 while True:
-    bus.write_byte(address,A0)
+    bus.write_byte(address, A0)
     value = bus.read_byte(address)
     print("Measured voltage: {:0.3f}".format(value / 255 * input_voltage))
     time.sleep(0.1)
