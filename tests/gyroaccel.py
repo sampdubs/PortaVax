c3dee37620cf0d1d8c5e977f5e1424f492ecbfe9
Sam Prausnitz-Weinbaum <sammypw1@gmail.com>
2022-06-30 15:11:54 +0100

    Add initial zeroing of measurements

--- tests/gyroaccel.py
+++ tests/gyroaccel.py
@@ -1,8 +1,7 @@
+# Based on:
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-# Simple demo of the LSM9DS0 accelerometer, magnetometer, gyroscope.
-# Will print the acceleration, magnetometer, and gyroscope values every second.
 import time
 import os
 
@@ -12,10 +11,12 @@ import busio
 import adafruit_lsm9ds0
 
 class MovingAverage():
-    def __init__(self, readings=100):
+    def __init__(self, readings=100, needsZero=False):
         self.data = []
         self.readings = readings
         self.ready = False
+        self.offset = 0
+        self.zeroed = not needsZero
     
     def update(self, datum):
         if len(self.data) < self.readings:
@@ -25,27 +26,35 @@ class MovingAverage():
             self.data.append(datum)
             self.ready = True
     
+    def zero(self):
+        self.offset = self._average()
+        self.zeroed = True
+    
     def _average(self):
-        return sum(self.data) / len(self.data)
+        return sum(self.data) / len(self.data) - self.offset
 
     def __str__(self):
-        if self.ready:
+        if self.ready and self.zeroed:
             return str(self._average())
         return "..."
     
     def __format__(self, spec):
-        if self.ready:
+        if self.ready and self.zeroed:
             return "{:0.3f}".format(self._average())
         return "..."
 
 class MovingAverage3D():
-    def __init__(self, readings=100):
-        self.data = [MovingAverage(readings) for _ in range(3)]
+    def __init__(self, readings=100, needsZero=False):
+        self.data = [MovingAverage(readings, needsZero) for _ in range(3)]
 
     def update(self, datum):
         datum = list(datum)
         for i in range(3):
             self.data[i].update(datum[i])
+    
+    def zero(self):
+        for ma in self.data:
+            ma.zero()
     
     def _average(self):
         return [ma._average() for ma in self.data]
@@ -55,21 +64,17 @@ class MovingAverage3D():
     
     def __format__(self, spec):
         return str(self)
-    
 
 # I2C connection:
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
 
 temp = MovingAverage()
-accel = MovingAverage3D()
-mag = MovingAverage3D()
-gyro = MovingAverage3D()
+accel = MovingAverage3D(needsZero=True)
+mag = MovingAverage3D(needsZero=True)
+gyro = MovingAverage3D(needsZero=True)
 
-
-# Main loop will read the acceleration, magnetometer, gyroscope, Temperature
-# values every second and print them out.
-while True:
+def main():
     # Read acceleration, magnetometer, gyroscope, temperature.
     accel.update(sensor.acceleration)
     mag.update(sensor.magnetic)
@@ -81,5 +86,16 @@ while True:
     print("Magnetometer (gauss):\t\t{}".format(mag))
     print("Gyroscope (degrees/sec):\t{}".format(gyro))
     print("Temperature:\t\t\t{}C".format(temp))
-    # Delay for a second.
-    # time.sleep(0.2)
+
+if __name__ == "__main__":
+    for _ in range(400):
+        main()
+
+    accel.zero()
+    mag.zero()
+    gyro.zero()
+
+    # Main loop will read the acceleration, magnetometer, gyroscope, Temperature
+    # values every second and print them out.
+    while True:
+        main()
