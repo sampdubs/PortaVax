89812db2872a9daa18f3d66a19f7b50b64b8e2ce
Sam Prausnitz-Weinbaum <sammypw1@gmail.com>
2022-06-30 16:58:00 +0100

    Add voltage measurement test

---
+++ tests/voltage.py
@@ -0,0 +1,14 @@
+import smbus
+import time
+
+address = 0x48
+A0 = 0x40
+bus = smbus.SMBus(1)
+
+input_voltage = 5
+
+while True:
+    bus.write_byte(address,A0)
+    value = bus.read_byte(address)
+    print("Measured voltage: {:0.3f}".format(value / 255 * input_voltage))
+    time.sleep(0.1)
